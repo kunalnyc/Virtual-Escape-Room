dca087b54f9ea18f0a80413684fbec6cf7bba91c
Kunal <kunalkmr9717@outlook.com>
2024-05-18 10:48:04 +0530

    Some Changes

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './auth/Login';
 import Register from './auth/Register';
 import GameRoom from './room/GameRoom';
-
+import './App.css';
 function App() {
   return (
     <Router>
+      <div className="App">
       <Routes>
-        <Route path="/auth/login" element={<Login />} />
-        <Route path="/auth/register" element={<Register />} />
-        <Route path="/room/gameroom" element={<GameRoom />} />
-        <Route path="/" exact element={<div>Home</div>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/gameroom" element={<GameRoom />} />
+        <Route path="/" exact element={<div>Home is fun</div>} />
       </Routes>
+      </div>
     </Router>
   );
 }
